2d30b434fe1d95c77ac5e7ec781dabf2618248fa
gnieto11 <maurogonzalonieto2011@hotmail.com.ar>
2020-02-20 18:33:37 -0300

    [Agrego router]

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <About msg="Este es mi portafolio"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import About from './components/About.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    About
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/router.js'
+import Vuetify from 'vuetify/lib'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify)
 
 new Vue({
+  router,
+  vuetify: new Vuetify({
+  }),
   render: h => h(App),
 }).$mount('#app')
