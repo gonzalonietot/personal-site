8ad098ad99a252c4fbed9a8c7184fa4d39f6be3e
gnieto11 <maurogonzalonieto2011@hotmail.com.ar>
2020-02-21 18:36:32 -0300

    cambio fondo

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <h1 class="name">Mauro Gonzalo Nieto</h1>
         <h2 class="software-developer">Software developer</h2>
     </div>
 </template>
@@ -13,20 +14,25 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     div {
-        background-image: url("https://images3.alphacoders.com/228/thumb-1920-228417.jpg");
-        padding: 350px;
+        background-image: url("https://www.fondosni.com/images/2012-10-21/Globo%20sobre%20el%20mundo-510880.jpg");
+        height: 730px;
+        max-height: 800px;
         background-position: center;
+    }
+    .name {
+        color: white;
+        text-align: center;
+        font-size: 300%;
+        transform: translate(1%, 200%);
+        margin-left: 30%;
+        margin-right: 30%;
     }
     .software-developer {
         color: white;
         border: 1px white solid;
-        text-align: center
-    }
-
-    .centrado {
-        position: relative;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+        text-align: center;
+        transform: translate(1%, 500%);
+        margin-left: 40%;
+        margin-right: 40%;
     }
 </style>
