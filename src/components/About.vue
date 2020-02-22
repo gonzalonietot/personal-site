d93da123953ce48d4fc7bdad31340dd4ae2dd55c
gnieto11 <maurogonzalonieto2011@hotmail.com.ar>
2020-02-22 11:39:47 -0300

    Agrego contenido al boton sobre mi

---
+++ src/components/About.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <v-dialog v-model="showAbout" persistent max-width="600px">
+            <v-card class="about-image-background">
+                <v-card-title>
+                    <span class="headline" style="color:black">ACERCA DE MI</span>
+                </v-card-title>
+                <v-card-text>
+                    <ul class="list">
+                        <li>Fecha de nacimiento: 01/11/1997</li>
+                        <li>Nacionalidad: Argentina</li>
+                        <li>Celular: (0351)- 3516745259</li>
+                        <li>E-mail: maurogonzalonieto2011@hotmail.com.ar</li>
+                    </ul>
+                    <br>
+                    <p class="about">Soy programador full stack, me desenvuelvo en tecnologías, tales como
+                        HTML, CSS, Javascript, Vue.js, Node.js, Docker, MySql, MongoDB. Me considero
+                        entusiasta con ganas de aprender nuevas tecnologías, dispuesto a enfrentar nuevos proyectos,
+                        que brinden soluciones a la comunidad.</p>
+                    <br>
+                    <small><strong>*By Gonzalo Nieto</strong></small>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn text @click="closeAboutMe()">Cerrar</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'About',
+    props: {
+      showAbout: {
+          type: Boolean,
+          default: false
+        },
+    },
+    methods: {
+      closeAboutMe () {
+        this.$emit('update:showAbout',false)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .about-image-background {}
+    .list {
+        margin-left: 15px;
+        font-size: 15px;
+        color: black
+    }
+    .about {
+        color: black;
+        font-size: 16px
+    }
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,24 +1,39 @@
 <template>
-    <div>
+    <div class="image-background">
         <h1 class="name">Mauro Gonzalo Nieto</h1>
         <h2 class="software-developer">Software developer</h2>
         <template style="border: 1px white solid;">
-            <v-btn height="35" width="150" class="about-me" outlined color="#FFFFFF">Sobre mi</v-btn>
+            <v-btn height="35" width="150" class="about-me" outlined color="#FFFFFF" @click="openAboutMe()">Sobre mi</v-btn>
             <v-btn height="35" width="150" class="technologies" outlined color="#FFFFFF">Tecnologías</v-btn>
             <v-btn height="35" width="150" class="contact" outlined color="#FFFFFF">Contacto</v-btn>
         </template>
+        <about v-if="showAbout" :showAbout.sync="showAbout" />
     </div>
 </template>
 
 <script>
+import about from '../components/About.vue'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    about
+  },
+  data() {
+    return {
+      showAbout: false
+    }
+  },
+  methods: {
+    openAboutMe () {
+      this.showAbout = true
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    div {
+<style lang="scss" scoped>
+    .image-background {
         background-image: url("https://www.fondosni.com/images/2012-10-21/Globo%20sobre%20el%20mundo-510880.jpg");
         height: 730px;
         max-height: 800px;
@@ -41,15 +56,15 @@ export default {
         margin-right: 40%;
     }
     .about-me {
-        transform: translate(1%, 500%);
+        transform: translate(1%, 520%);
         margin-left: 45%;
     }
     .technologies {
-        transform: translate(1%, 550%);
+        transform: translate(1%, 590%);
         margin-left: 45%;
     }
     .contact {
-        transform: translate(1%, 600%);
+        transform: translate(1%, 660%);
         margin-left: 45%;
     }
 </style>
