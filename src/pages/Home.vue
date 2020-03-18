a1233a167811f5eb595acd44dd60584f5a15ce0b
gnieto11 <maurogonzalonieto2011@hotmail.com.ar>
2020-03-18 17:57:31 -0300

    Agrego footer

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,42 @@
 <template>
     <div class="image-background">
-        <h3 class="software-developer">{{softwareDeveloper}}</h3>
-        <template v-if="!showAbout && !showTechnologies && !showContact" >
-            <button type="button" class="about-me button" @click="openAboutMe()">{{aboutMe}}</button>
-            <button type="button" class="technologies button" @click="openTechnologies()">{{technologies}}</button>
-          <button type="button" class="contact button" @click="openContact()">{{contact}}</button>
-        </template>
-        <about v-if="showAbout" :showAbout.sync="showAbout" />
-        <technologies v-if="showTechnologies" :show-technologies.sync="showTechnologies" />
-        <contact v-if="showContact" :show-contact.sync="showContact" />
+      <h3 class="software-developer">{{softwareDeveloper}}</h3>
+      <template v-if="!showAbout && !showTechnologies && !showContact" >
+        <button type="button" class="about-me button" @click="openAboutMe()">{{aboutMe}}</button>
+        <button type="button" class="technologies button" @click="openTechnologies()">{{technologies}}</button>
+        <button type="button" class="contact button" @click="openContact()">{{contact}}</button>
+      </template>
+      <v-footer
+        v-if="!showAbout && !showTechnologies && !showContact"
+        class="footer"
+        dark
+        padless
+      >
+        <v-card
+          class="flex"
+          flat
+          tile
+        >
+          <v-card-title class="teal">
+            <strong class="subheading">{{connect}}</strong>
+            <v-spacer></v-spacer>
+            <v-btn
+              v-for="icon in icons"
+              :key="icon"
+              class="mx-4"
+              dark
+              icon>
+              <v-icon size="24px">{{ icon }}</v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-card-text class="py-2 white--text text-center">
+            {{ new Date().getFullYear() }} — <strong>{{allRightsReserved}}<br>{{fullName}}</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
+      <about v-if="showAbout" :showAbout.sync="showAbout" />
+      <technologies v-if="showTechnologies" :show-technologies.sync="showTechnologies" />
+      <contact v-if="showContact" :show-contact.sync="showContact" />
     </div>
 </template>
 
@@ -28,11 +56,20 @@ export default {
       showAbout: false,
       showTechnologies: false,
       showContact: false,
-      name: 'Mauro Gonzalo Nieto',
+      fullName: 'Gonzalo Nieto',
       aboutMe: 'Sobre mi',
       technologies: 'Tecnologías',
       contact: 'Contacto',
-      softwareDeveloper: 'Software developer'
+      softwareDeveloper: 'Software developer',
+      connect: 'Conectate a mis redes sociales',
+      allRightsReserved: 'Todos los derechos reservados',
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-google-plus',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
     }
   },
   methods: {
@@ -84,6 +121,14 @@ export default {
         height:40px ;
         width: 150px;
     }
+    .footer {
+      background-color: black;
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      height: 40px;
+      color: white;
+    }
 
     @media screen and (max-width: 768px) {
         .image-background {
@@ -108,9 +153,9 @@ export default {
             margin-left: 30%;
             transform: translate(1%, 750%);
         }
-      .contact {
-        transform: translate(1%, 800%);
-        margin-left: 30%;
+        .contact {
+          transform: translate(1%, 800%);
+          margin-left: 30%;
       }
     }
 </style>
